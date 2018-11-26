736646850033941111436158ccbf1920cecb5f79
tts-walid <Walid@techtalentsouth.com>
2018-11-26 18:58:57 -0500

    Update add_it_up.rb

--- ruby/add_it_up.rb
+++ ruby/add_it_up.rb
@@ -2,8 +2,8 @@
 #add_it_up is the method name
 #(num1, hi, hello) are the arguments. Their names don't matter.
 #To call a method you need to include the appropiate number of arguments. If you told rubythat it takes 10 args pass 10 to it
-def add_it_up(num1, hi, hello)
-  hi + hello + num1
+def add_it_up(num100, hi, hello)
+  hi + hello + num100
 end
 
-puts add_it_up(number, number2, number3)
+puts add_it_up(number1, number2, number3)
